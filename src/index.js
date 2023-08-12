56e5cb355b3b65924687a0ff7b77deb53efd443f
Karisma Sousa <SOUSAKARISMA@GMAIL.COM>
2023-08-11 21:23:10 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './index.css';
+import React from 'react';
+
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+
+import App from './views/App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+    <BrowserRouter>
+    <App tab="home"/>
+    </BrowserRouter>
+        
+);
